e9476dfbe361da1052fb95e60b8e030722f30f08
Robert mackie <robert@allmakes.co.uk>
2018-03-07 11:28:37 +0000

    Changed Comments

--- MackieeE/SidebarWeight/registration.php
+++ MackieeE/SidebarWeight/registration.php
@@ -1,25 +1,10 @@
 <?php
 /**
- * Allmakes
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Mageplaza.com license that is
- * available through the world-wide-web at this URL:
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
- *
- * @author 		Robert M
- * @category    Allmakes
- * @package     Allmakes_Checkout
- * @copyright   Copyright (c) 2017 Allmakes (http://www.allmakes4x4.com/)
+ * @author Robert M
  */ 
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Allmakes_Checkout',
+	'MackieeE/SidebarWeight',
 	__DIR__
 	);
 ?>
